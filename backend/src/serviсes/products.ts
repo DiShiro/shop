6e289bafef5c346918ae65a6fadb568fab8e0064
DiShiro <q15112007@gmail.com>
2026-03-13 11:46:36 +0300

    services_commit

---
+++ backend/src/serviсes/products.ts
@@ -0,0 +1,29 @@
+import prisma from '../db';
+import { Product, Prisma } from '../generated/prisma';
+
+export async function getAllProducts() {
+  return prisma.product.findMany();
+}
+
+export async function getProductById(id: number) {
+    return prisma.product.findMany();
+}
+
+export async function createProduct(data: Prisma.ProductCreateInput){
+    return prisma.product.create({
+    data,
+  });
+}
+
+export async function updateProduct(id: number, data: Prisma.ProductUpdateInput) {
+  return prisma.product.update({
+    where: { id },
+    data,
+  });
+}
+
+export async function deleteProduct(id: number) {
+  return prisma.product.delete({
+    where: { id },
+  });
+}
